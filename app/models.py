6118a4a65be10458900debabcac0d24381f44ebb
manuel <neozeroes@gmail.com>
2019-10-24 13:12:29 -0600

    Adding db user management

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from datetime import datetime
 from app import db
 
@@ -26,3 +27,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body) 
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
